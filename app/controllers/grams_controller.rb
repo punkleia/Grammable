a4bcffecbb32eecbbd36e37f5502115ee487fbe0
Michelle Luby <theatrejawa@yahoo.com>
2017-04-03 00:07:28 +0000

    added update function and tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,29 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def index
   end
 
   def new
     @gram = Gram.new
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
 
   def create
@@ -20,15 +38,17 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-      if @gram.blank?
-        render plain: "Not Found :(", status: :not_found
-      end
+    return render_not_found if @gram.blank?
   end
 
 private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: "Not Found :(", status: :not_found
   end
 
 end
